73f9767e454d3d3bf977c6cabb378ba228b06b9c
modmuss50 <modmuss50@gmail.com>
2017-04-21 22:41:49 +0100

    A mod I guess. If you can call it that

---
+++ src/main/java/teamreborn/techreborn/TechReborn.java
@@ -0,0 +1,8 @@
+package teamreborn.techreborn;
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod(modid = "techreborn", name = "Tech Reborn", version = "@MODVERSION@")
+public class TechReborn {
+
+}
