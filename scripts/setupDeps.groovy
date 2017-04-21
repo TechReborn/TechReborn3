941257887ac6ef7fd3367a4ca7a031d7acf211f2
modmuss50 <modmuss50@gmail.com>
2017-04-21 23:10:42 +0100

    Add setupAll task

--- scripts/setupDeps.groovy
+++ scripts/setupDeps.groovy
@@ -1,9 +1,33 @@
-task ("setupDeps") {
+task ("setupAll") {
     def cacheFiles = new File("Reborncore")
     if(!cacheFiles.exists()){
         logger.lifecycle(":Cloning RebornCore")
+        exec 
+        { 
+            executable 'git' 
+            args 'clone', 'https://github.com/TechReborn/RebornCore.git'
+        }
+
+        exec 
+        {
+            workingDir 'Reborncore'
+            executable 'git' 
+            args 'checkout', '1.11.2-rewrite'
+        }
+
     } else {
         logger.lifecycle(":Pulling RebornCore")
+        exec 
+        {
+            workingDir 'Reborncore'
+            executable 'git' 
+            args 'pull'
+        }
     }
-    
+    idea.module.sourceDirs += sourceSets.RebornCore.java.srcDirs
+    idea.module.sourceDirs += sourceSets.RebornCore.resources.srcDirs
 }
+
+
+tasks.idea.dependsOn "setupDecompWorkspace"
+setupAll.finalizedBy(tasks.idea)
--- src/main/java/teamreborn/techreborn/TechReborn.java
+++ src/main/java/teamreborn/techreborn/TechReborn.java
@@ -5,4 +5,7 @@ import net.minecraftforge.fml.common.Mod;
 @Mod(modid = "techreborn", name = "Tech Reborn", version = "@MODVERSION@")
 public class TechReborn {
 
+	public static void main(String[] args) {
+		System.out.println();
+	}
 }
