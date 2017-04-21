f27ec132222be198aa0c54ffb9015a7f986a49f3
modmuss50 <modmuss50@gmail.com>
2017-04-22 00:13:25 +0100

    Simplify the setupAll task, should fix the issues

---
+++ setupAll.bat
@@ -0,0 +1,2 @@
+git clone -b 1.11.2-rewrite https://github.com/TechReborn/RebornCore.git
+gradlew setupAll
--- src/main/java/teamreborn/techreborn/TechReborn.java
+++ src/main/java/teamreborn/techreborn/TechReborn.java
@@ -1,11 +1,12 @@
 package teamreborn.techreborn;
 
 import net.minecraftforge.fml.common.Mod;
+import teamreborn.reborncore.Constants;
 
 @Mod(modid = "techreborn", name = "Tech Reborn", version = "@MODVERSION@")
 public class TechReborn {
 
 	public static void main(String[] args) {
-		System.out.println();
+		System.out.println(Constants.MODID);
 	}
 }
